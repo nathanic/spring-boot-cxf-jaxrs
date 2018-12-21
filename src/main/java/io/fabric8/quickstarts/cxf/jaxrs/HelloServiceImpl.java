2889b8d5e33df3a605b5604fd882bbba6fb5d8d2
Nathan Stien <nathanism@gmail.com>
2018-12-21 15:38:26 -0600

    showing off changes

--- src/main/java/io/fabric8/quickstarts/cxf/jaxrs/HelloServiceImpl.java
+++ src/main/java/io/fabric8/quickstarts/cxf/jaxrs/HelloServiceImpl.java
@@ -25,7 +25,7 @@ public class HelloServiceImpl implements HelloService {
     }
 
     public String sayHello(String a) {
-        return "Hello " + a + ", Welcome to CXF RS Spring Boot and such";
+        return "Hello " + a + ", Welcome to CXF RS Spring Boot and such.  Merry Christmas";
     }
     
 }
